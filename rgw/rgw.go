f0588a0ea8a584a0474f25acb62e859efe5c9f0b
Yang Honggang <rtlinux@163.com>
2020-12-04 23:38:25 +0800

    Mount/Umount/GetRootFileHandle/StatFS

--- rgw/rgw.go
+++ rgw/rgw.go
@@ -51,8 +51,9 @@ type FS struct {
 
 // int rgw_mount(librgw_t rgw, const char *uid, const char *key,
 //               const char *secret, rgw_fs **fs, uint32_t flags)
-func (fs *FS) Mount(rgw *RGW, uid, key, secret *C.char, flags C.uint) error {
-	ret := C.rgw_mount(*rgw.libRGW, uid, key, secret, &fs.rgwFS, flags)
+func (fs *FS) Mount(rgw *RGW, uid, key, secret string, flags uint32) error {
+	ret := C.rgw_mount(*rgw.libRGW, C.CString(uid), C.CString(key),
+		C.CString(secret), &fs.rgwFS, C.uint(flags))
 	if ret != 0 {
 		return getError(ret)
 	}
@@ -61,11 +62,73 @@ func (fs *FS) Mount(rgw *RGW, uid, key, secret *C.char, flags C.uint) error {
 }
 
 // int rgw_umount(rgw_fs *fs, uint32_t flags)
-func (fs *FS) Umount(flags C.uint) error {
-	if ret := C.rgw_umount(fs.rgwFS, flags); ret == 0 {
+func (fs *FS) Umount(flags uint32) error {
+	if ret := C.rgw_umount(fs.rgwFS, C.uint(flags)); ret == 0 {
 		return nil
 	} else {
 		return getError(ret)
 	}
 
+}
+
+// StatVFS instances are returned from the StatFS call. It reports
+// file-system wide statistics.
+type StatVFS struct {
+	// Bsize reports the file system's block size.
+	Bsize int64
+	// Fragment reports the file system's fragment size.
+	Frsize int64
+	// Blocks reports the number of blocks in the file system.
+	Blocks uint64
+	// Bfree reports the number of free blocks.
+	Bfree uint64
+	// Bavail reports the number of free blocks for unprivileged users.
+	Bavail uint64
+	// Files reports the number of inodes in the file system.
+	Files uint64
+	// Ffree reports the number of free indoes.
+	Ffree uint64
+	// Favail reports the number of free indoes for unprivileged users.
+	Favail uint64
+	// Fsid reports the file system ID number.
+	Fsid [2]int64
+	// Flag reports the file system mount flags.
+	Flag int64
+	// Namemax reports the maximum file name length.
+	Namemax int64
+}
+
+// struct rgw_file_handle
+type FileHandle struct {
+	handle *C.struct_rgw_file_handle
+}
+
+func (fs *FS) GetRootFileHandle() *FileHandle {
+	return &FileHandle{
+		handle: fs.rgwFS.root_fh,
+	}
+}
+
+//    int rgw_statfs(rgw_fs *fs, rgw_file_handle *parent_fh,
+//                   rgw_statvfs *vfs_st, uint32_t flags)
+func (fs *FS) StatFS(pFH *FileHandle, flags uint32) (*StatVFS, error) {
+	var statVFS C.struct_rgw_statvfs
+	if ret := C.rgw_statfs(fs.rgwFS, pFH.handle, &statVFS, C.uint(flags)); ret != 0 {
+		return nil, getError(ret)
+	} else {
+		stat := &StatVFS{
+			Bsize:   int64(statVFS.f_bsize),
+			Frsize:  int64(statVFS.f_frsize),
+			Blocks:  uint64(statVFS.f_blocks),
+			Bfree:   uint64(statVFS.f_bfree),
+			Bavail:  uint64(statVFS.f_bavail),
+			Files:   uint64(statVFS.f_files),
+			Ffree:   uint64(statVFS.f_ffree),
+			Favail:  uint64(statVFS.f_favail),
+			Fsid:    [2]int64{int64(statVFS.f_fsid[0]), int64(statVFS.f_fsid[1])},
+			Flag:    int64(statVFS.f_flag),
+			Namemax: int64(statVFS.f_namemax),
+		}
+		return stat, nil
+	}
 }
--- rgw/rgw_test.go
+++ rgw/rgw_test.go
@@ -1,14 +1,37 @@
 package rgw
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+/*
 func TestCreateRGW(t *testing.T) {
 	rgw, err := CreateRGW([]string{"-c /etc/ceph/ceph.conf", "--name client.rgw.bjlt03-e57"})
 	assert.NoError(t, err)
 	assert.NotNil(t, rgw)
+	ShutdownRGW(rgw)
+}
+*/
+
+func TestMountUmount(t *testing.T) {
+	rgw, err := CreateRGW([]string{"-c /etc/ceph/ceph.conf", "--name client.rgw.bjlt03-e57"})
+	assert.NoError(t, err)
+	assert.NotNil(t, rgw)
+
+	fs := FS{}
+	err = fs.Mount(rgw, "test", "ak", "sk", 0)
+	assert.NoError(t, err)
+
+	statVFS, err := fs.StatFS(fs.GetRootFileHandle(), 0)
+	assert.NotNil(t, statVFS)
+	assert.NoError(t, err)
+	fmt.Println(">>>> statVFS.Blocks ", statVFS.Blocks)
+
+	err = fs.Umount(0)
+	assert.NoError(t, err)
+
 	ShutdownRGW(rgw)
 }
